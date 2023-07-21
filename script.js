f0954d543b7e4b1011c964da14465a50aba063c0
Emre Sahin <sahinemre899@gmail.com>
2023-07-21 15:23:35 +0300

    My First Js App

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
+
+
+function addNote(){
+    if(inputBox.value === ''){
+        alert("Note Can not be empty");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = ''
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("selected");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function getData(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+getData();
